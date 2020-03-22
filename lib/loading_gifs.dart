e76e50e30588da6249639bd1c30c64e34ecf6465
Ray Li <ray.li.verified@gmail.com>
2020-03-22 13:54:13 -0500

    Cleanup Assets
    *Generate updated version of loading indicators.

--- lib/loading_gifs.dart
+++ lib/loading_gifs.dart
@@ -1,1 +1,6 @@
 library loading_gifs;
+
+const String cupertinoActivityIndicator =
+    "packages/loading_gifs/assets/images/cupertino_activity_indicator.gif";
+const String circularProgressIndicator =
+    "packages/loading_gifs/assets/images/circular_progress_indicator.gif";
